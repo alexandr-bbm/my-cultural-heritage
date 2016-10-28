e957f8d3cd637dcfa93d3fd96d3663e8b4ed7560
Alexander <alexandr.bbm@gmail.com>
2016-10-28 18:48:09 +0700

    test modal

--- frontend/src/api/getObjects/index.js
+++ frontend/src/api/getObjects/index.js
@@ -1,13 +1,16 @@
+import MOCK_OBJECTS from 'api/mock';
+
 export default function getObjects() {
     return new Promise((resolve, reject) => {
         $.ajax({
             url: 'http://heritage.pythonanywhere.com/objects/?format=json',
             type: 'GET',
             success: function (data) {
-                resolve({
-                    data
-                });
-            }
+                resolve(data);
+            },
+            error: function () {
+                resolve(MOCK_OBJECTS)
+            },
         })
     })
 }
