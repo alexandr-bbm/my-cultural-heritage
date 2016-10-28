ef4e85538e7a8c16b4287bcbc9e987a69d5efde7
Artem Kanarev <artem.kanarev@invitae.com>
2016-10-28 19:03:11 +0700

    admin search

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -28,5 +28,5 @@ router.register(r'rating', RatingViewSet)
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
-    url(r'^', TemplateView.as_view(template_name='index.html'))
+    url(r'^$', TemplateView.as_view(template_name='index.html'))
 ]
