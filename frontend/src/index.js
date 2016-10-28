d228bb1d184bcde8936b3cc3680ad7e8f6f75c3f
Alexander <alexandr.bbm@gmail.com>
2016-10-28 14:25:56 +0700

    replaced paths

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const wrappedApp =
+    <MuiThemeProvider>
+        <App />
+    </MuiThemeProvider>;
 
 import './styles/main.scss';
 
-ReactDOM.render(<App className="app" />, document.getElementById('content-entry'));
+ReactDOM.render(wrappedApp, document.getElementById('content-entry'));
