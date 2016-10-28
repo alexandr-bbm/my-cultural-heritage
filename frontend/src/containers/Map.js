ca20959a9b43c36528027df62d9b274007f37c55
Глеб Вениаминович <tempuro1990@gmail.com>
2016-10-28 14:14:59 +0700

    sctrucrure

---
+++ frontend/src/containers/Map.js
@@ -0,0 +1,19 @@
+import React, {
+    Component,
+    PropTypes,
+} from 'react';
+
+class Map extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+Map.propTypes = {};
+Map.defaultProps = {};
+
+export default Map;
